d8e563967dd172003cfe08e526f456ab7ef4c5af
Locatelli16 <164167466+Locatelli16@users.noreply.github.com>
2024-10-16 09:24:58 -0300

    Contador

---
+++ script.js
@@ -0,0 +1,11 @@
+let valorContador = 0;
+
+function incrementar() {
+    valorContador ++;
+    document.getElementByld('contador').innerText = valorContador;
+}
+
+function diminuir() {
+    valorContador --;
+    document.getElementByld('contador').innerText = valorContador;
+}
